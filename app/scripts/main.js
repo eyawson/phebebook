6eb6c3834defe42f1647c4fd9b223c9a70847827
eyawson <ebow.yawson@gmail.com>
2019-09-05 02:42:26 -0400

    add feature to toggle about author and book info

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,26 +1,31 @@
-const start = () => {
-    console.log('\'Phebe\' \'Yawson!\'');
-
+const runJS = () => {
     document.getElementById('show').style.display = 'none';
+
     const summary = 'Tyese is known for being reserved and quiet, but when she accidentally peels back the facade from her family secrets she must rise up to lead her friends and family in a war against an ancient evil. While fighting for the future of all she holds dear, she must also battle ghosts from her past and her changing identity. Not just anyone can be The Alpha, and she never expected to be anything but human.';
 
-    const aboutHer ='In 2002 Phebe Yawson volunteered her services to a non-profit in Ghana, West Africa after graduating from DeVry. In 2015 she relocated back to America to raise her family. Now able to pursue her dreams she spends her days with her husband and kids and her nights with her pen. Phebe is now an award winning author and poet.'
-
     document.getElementById('test').innerHTML = summary;
+
+    const aboutHer ='In 2002 Phebe Yawson volunteered her services to a non-profit in Ghana, West Africa after graduating from DeVry. In 2015 she relocated back to America to raise her family. Now able to pursue her dreams she spends her days with her husband and kids and her nights with her pen. Phebe is now an award winning author and poet.';
+
+    $(document).ready(() => {
+        $('#hide').on('click',() => {
+            $('main, footer, .fab, .fas').hide();
+            $('#show').show();
+            $('#hide').hide()
+        });
+        $('#show').on('click',() => {
+            $('main, footer, .fab, .fas').show();
+            $('#show').hide();
+            $('#hide').show();
+        });
+        $('#about').on('click',() => {
+            if ($('p#test').text() == summary) { 
+                $('p#test').text(aboutHer); 
+            } else { 
+                $('p#test').text(summary); 
+            }
+        });
+    });
 }
-start();
 
-
-$(document).ready(function(){
-    $('#hide').click(function(){
-      $('main, footer, .fab, .fas').hide();
-      $('#show').show();
-      $('#hide').hide()
-    });
-    $('#show').click(function(){
-      $('main, footer, .fab, .fas').show();
-      $('#show').hide();
-      $('#hide').show();
-    });
-  });
-
+runJS();
