b391aba5c929b47c67f488dd96742fb4587ae2c5
eyawson <ebow.yawson@gmail.com>
2019-09-16 16:26:01 -0400

    switched from image background to white

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,15 @@
 const runJS = () => {
-    document.getElementById('show').style.display = 'none';
-
-    const summary = 'Tyese is known for being reserved and quiet, but when she accidentally peels back the facade from her family secrets she must rise up to lead her friends and family in a war against an ancient evil. While fighting for the future of all she holds dear, she must also battle ghosts from her past and her changing identity. Not just anyone can be The Alpha, and she never expected to be anything but human.';
-
-    document.getElementById('test').innerHTML = summary;
+    const synopsis = 'Tyese is known for being reserved and quiet, but when she accidentally peels back the facade from her family secrets she must rise up to lead her friends and family in a war against an ancient evil. While fighting for the future of all she holds dear, she must also battle ghosts from her past and her changing identity. Not just anyone can be The Alpha, and she never expected to be anything but human.';
 
     const aboutHer ='In 2002 Phebe Yawson volunteered her services to a non-profit in Ghana, West Africa after graduating from DeVry. In 2015 she relocated back to America to raise her family. Now able to pursue her dreams she spends her days with her husband and kids and her nights with her pen. Phebe is now an award winning author and poet.';
 
-    $(document).ready(() => {
+    const summary = document.getElementById('synopsis');
+    summary.innerHTML = synopsis;
+
+    const aboutPhebe = document.getElementById('aboutPhebe');
+    aboutPhebe.innerHTML = aboutHer;
+
+    /* $(document).ready(() => {
         $('#hide').on('click',() => {
             $('main, footer, .fab, .fas').hide();
             $('#show').show();
@@ -25,7 +27,7 @@ const runJS = () => {
                 $('p#test').text(summary); 
             }
         });
-    });
+    }); */
 }
 
 runJS();
