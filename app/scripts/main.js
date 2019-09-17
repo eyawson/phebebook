67ac91c7dafbc186b827aa8aaff342649321777a
eyawson <ebow.yawson@gmail.com>
2019-09-16 23:42:09 -0400

    added borders to elements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,7 @@
 const runJS = () => {
     const synopsis = 'Tyese is known for being reserved and quiet, but when she accidentally peels back the facade from her family secrets she must rise up to lead her friends and family in a war against an ancient evil. While fighting for the future of all she holds dear, she must also battle ghosts from her past and her changing identity. Not just anyone can be <strong>The Alpha</strong>, and she never expected to be anything but human.';
 
-    const aboutHer ='In 2002 Phebe Yawson volunteered her services to a non-profit in Ghana, West Africa after graduating from DeVry. In 2015 she relocated back to America to raise her family. Now able to pursue her dreams she spends her days with her husband and kids and her nights with her pen. Phebe is now an award winning author and poet.';
+    const aboutHer ='In 2002 Phebe Yawson volunteered her services to a non-profit in Ghana, West Africa after graduating from DeVry. In 2015, she relocated back to America to raise her family. Now able to pursue her dreams she spends her days with her husband and kids and her nights with her pen. Phebe is now an award winning author and poet.';
 
     const summary = document.getElementById('synopsis');
     summary.innerHTML = synopsis;
